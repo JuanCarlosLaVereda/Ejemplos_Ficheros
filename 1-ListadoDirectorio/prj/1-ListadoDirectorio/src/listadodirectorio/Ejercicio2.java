84206972693bfecc73912ea0065f5c4a2f36dd57
JuanCarlosLaVereda <juasanper2@alu.edu.gva.es>
2024-09-12 21:16:47 +0200

    Ejercicio 2 por terminar

--- 1-ListadoDirectorio/prj/1-ListadoDirectorio/src/listadodirectorio/Ejercicio2.java
+++ 1-ListadoDirectorio/prj/1-ListadoDirectorio/src/listadodirectorio/Ejercicio2.java
@@ -1,7 +1,25 @@
 package listadodirectorio;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Ejercicio2 {
     public static void main(String[] args) {
+        String directorioBase = "/home/jsantamaria/pruebas";
+
+        File file = new File(directorioBase+"/Directorio1");
+        file.mkdir();
+        File file1 = new File(file + "/Directorio2");
+        file1.mkdir();
+        File file2 = new File(file1 + "/Directorio3");
+        file2.mkdir();
+        File file3 = new File(file2);
+        try {
+            file3.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
